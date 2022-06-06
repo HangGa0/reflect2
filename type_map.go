c49dba8c1da9808e5a2300980d895609162d4540
gaohang.gh <gaohang.gh@BYTEDANCE.COM>
2022-06-06 15:34:46 +0800

    delete typebyname

--- type_map.go
+++ type_map.go
@@ -10,7 +10,7 @@ import (
 
 // typelinks2 for 1.7 ~
 //go:linkname typelinks2 reflect.typelinks
-func typelinks2() (sections []unsafe.Pointer, offset [][]int32)
+//func typelinks2() (sections []unsafe.Pointer, offset [][]int32)
 
 // initOnce guards initialization of types and packages
 var initOnce sync.Once
@@ -19,7 +19,7 @@ var types map[string]reflect.Type
 var packages map[string]map[string]reflect.Type
 
 // discoverTypes initializes types and packages
-func discoverTypes() {
+/*func discoverTypes() {
 	types = make(map[string]reflect.Type)
 	packages = make(map[string]map[string]reflect.Type)
 
@@ -47,24 +47,24 @@ func loadGoTypes() {
 		}
 	}
 }
-
+*/
 type emptyInterface struct {
 	typ  unsafe.Pointer
 	word unsafe.Pointer
 }
 
 // TypeByName return the type by its name, just like Class.forName in java
-func TypeByName(typeName string) Type {
+/*func TypeByName(typeName string) Type {
 	initOnce.Do(discoverTypes)
 	return Type2(types[typeName])
-}
+}*/
 
 // TypeByPackageName return the type by its package and name
-func TypeByPackageName(pkgPath string, name string) Type {
+/*func TypeByPackageName(pkgPath string, name string) Type {
 	initOnce.Do(discoverTypes)
 	pkgTypes := packages[pkgPath]
 	if pkgTypes == nil {
 		return nil
 	}
 	return Type2(pkgTypes[name])
-}
+}*/
